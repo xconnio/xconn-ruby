19ff0ca9bc9bdcb73ef0d2c11b4ec30981a97866
Ismail Akram <rubyonrails3@gmail.com>
2024-02-15 19:30:27 +0500

    Initial commit, setup workflow

---
+++ lib/wamp/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Wamp
+  VERSION = "0.1.0"
+end
---
+++ spec/wamp_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Wamp do
+  it "has a version number" do
+    expect(Wamp::VERSION).not_to be nil
+  end
+end
