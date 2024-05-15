9d3d6ee865430483ba23d444cfff8b4dc034c279
Ismail Akram <rubyonrails3@gmail.com>
2024-05-15 20:36:34 +0500

    Remove messages and authenticators from the repo

--- lib/wamp.rb
+++ lib/wamp.rb
@@ -6,8 +6,6 @@ require_relative "wamp/connection/session"
 require_relative "wamp/connection/websocket_connection"
 require_relative "wamp/serializer"
 require_relative "wamp/router"
-require_relative "wamp/message"
-require_relative "wamp/auth"
 
 module Wamp
   class Error < StandardError; end
